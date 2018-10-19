982b6f649b289b8d93ab3544b70473bf17f12ddc
张国召 <zhanggz@urwork.cn>
2018-10-19 14:50:14 +0800

    dev dev

--- Dev.java
+++ Dev.java
@@ -6,6 +6,8 @@
  **/
 public class Dev {
     public static void main(String[] args) {
+
         System.out.println("开发新dev功能，目前功能已经完成，暂时不上此功能，留到双十一上");
+        System.out.println("dev dev dev");
     }
 }
