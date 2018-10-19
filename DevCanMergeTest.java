27f19745fe060e380c2485039ded1d0663157f8a
张国召 <zhanggz@urwork.cn>
2018-10-19 11:16:05 +0800

    master commit:MasterTest.java

---
+++ DevCanMergeTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author: 张国召
+ * @create: 2018-10-19 11:15
+ * @Company: uw
+ * @description：
+ **/
+public class DevCanMergeTest {
+    public static void main(String[] args) {
+        System.out.println("Dev merge file");
+    }
+}
