d666641c6e1db760df504d7765e00fe6611c0554
张国召 <zhanggz@urwork.cn>
2018-10-19 11:28:56 +0800

    dev commit :other commit

---
+++ OtherDevTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author: 张国召
+ * @create: 2018-10-19 11:28
+ * @Company: uw
+ * @description：
+ **/
+public class OtherDevTest {
+    public static void main(String[] args) {
+        System.out.println("Other Dev");
+    }
+}
