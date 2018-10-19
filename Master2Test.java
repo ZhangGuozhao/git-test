c7b494cc4d3751c401fa9f0f8d27f11f885e94eb
张国召 <zhanggz@urwork.cn>
2018-10-19 11:53:08 +0800

    dev2commit

--- Master2Test.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author: 张国召
- * @create: 2018-10-19 11:41
- * @Company: uw
- * @description：
- **/
-public class Master2Test {
-}
--- Master3Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author: 张国召
- * @create: 2018-10-19 11:46
- * @Company: uw
- * @description：
- **/
-public class Master3Test {
-
-}
