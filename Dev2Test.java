eb5ee252495c1d50088a5be5400e0aa9948efb13
张国召 <zhanggz@urwork.cn>
2018-10-19 11:43:17 +0800

    Dev2Test commit

---
+++ Dev2Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author: 张国召
+ * @create: 2018-10-19 11:42
+ * @Company: uw
+ * @description：
+ **/
+public class Dev2Test {
+    public static void main(String[] args) {
+        System.out.println("Dev 2 Test");
+    }
+}
