21863acb3884d5db105da9f6fbd57e50b1748fe8
张国召 <zhanggz@urwork.cn>
2018-10-19 11:10:16 +0800

    master commit:MasterTest.java

---
+++ MasterTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author: 张国召
+ * @create: 2018-10-19 11:06
+ * @Company: uw
+ * @description：
+ **/
+public class MasterTest {
+    public static void main(String[] args) {
+        System.out.println("master: hello world!");
+    }
+}
