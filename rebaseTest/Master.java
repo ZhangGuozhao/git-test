c29be74f18f9e2184566a06f1f144a344dd24a71
zhangguozhao <wb_zhangguozhao@kuaishou.com>
2024-11-28 19:59:36 +0800

    B3: B3 rebase test
    on branc B3;
    git rebase master

--- rebaseTest/Master.java
+++ rebaseTest/Master.java
@@ -8,5 +8,6 @@ public class Master {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("master rebase");
+        System.out.println("B3");
     }
 }
