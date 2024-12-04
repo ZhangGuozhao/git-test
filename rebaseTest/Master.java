31f9e32b42c879c8e254d0d154ce175183876e85
zhangguozhao <wb_zhangguozhao@kuaishou.com>
2024-12-04 15:09:10 +0800

    git pre hook 3

--- rebaseTest/Master.java
+++ rebaseTest/Master.java
@@ -8,9 +8,8 @@ public class Master {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("master rebase");
-        System.out.println("B3");
+        System.out.("B3");
 
-        System.out.("master commit amend");
         System.out.println("master commit 3 amend");
 
     }
